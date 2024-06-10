905287e139d5e9bfe0d05b1b540f7a15e0d0e2a5
Jorge Matos <jisapjisap@gmail.com>
2024-06-10 17:49:05 +0200

    Esquema de validación

--- 01-reforzamiento_nest/src/config/envs.ts
+++ 01-reforzamiento_nest/src/config/envs.ts
@@ -1,9 +1,25 @@
 import 'dotenv/config';
+import * as joi from 'joi'
 
-// TODO: validar mediante un esquema
+interface EnvVars {
+  PORT: number;
 
+}
 
-export const envs = {
-  port: process.env.PORT
+const envsSchema = joi.object({                                        // Esquema de validación según joi
+  PORT: joi.number().required()
+})
+.unknown(true)
+
+const { error, value } = envsSchema.validate(process.env);             // Se le pasa a ese esquema la función de joi "validate"
+
+if(error){
+  throw new Error(`Config validation error: ${error.message}`);        // Si hay algún error dispara el throw   
+}
+
+const envVars:EnvVars = value;                                         // Tipamos las variables de entorno que usaremos
+
+export const envs = {                                                  // Y si no hay error se exporta la variable de entorno desde el esquema validado. 
+  port: envVars.PORT
 
 }
