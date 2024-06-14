83c53751a7709b88f8009d213397569413d45da4
Jorge Matos <jisapjisap@gmail.com>
2024-06-14 16:44:43 +0200

    update product

--- 02-products-app/products-ms/src/products/products.service.ts
+++ 02-products-app/products-ms/src/products/products.service.ts
@@ -55,7 +55,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+    return this.product.update({
+      where: { id },
+      data: updateProductDto
+    })
   }
 
   remove(id: number) {
