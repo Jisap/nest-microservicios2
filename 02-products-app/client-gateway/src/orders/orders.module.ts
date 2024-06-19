81148013af798017420d36346d2503a127a9e67b
Jorge Matos <jisapjisap@gmail.com>
2024-06-19 18:24:42 +0200

    Conectar gateway con OrderMicroservice

---
+++ 02-products-app/client-gateway/src/orders/orders.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { OrdersController } from './orders.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ORDER_SERVICE, envs } from 'src/config';
+
+@Module({
+  controllers: [OrdersController],
+  imports: [
+    ClientsModule.register([
+      {
+        name: ORDER_SERVICE,    // Injection token (nombre del servicio)
+        transport: Transport.TCP,
+        options: {
+          host: envs.ordersMicroservicesHost, // host
+          port: envs.ordersMicroservicesPort  // port
+        }
+      }
+    ])
+  ]
+  
+})
+export class OrdersModule {}
--- 02-products-app/orders-ms/src/orders/orders.controller.ts
+++ 02-products-app/orders-ms/src/orders/orders.controller.ts
@@ -20,7 +20,7 @@ export class OrdersController {
   }
 
   @MessagePattern('findOneOrder')
-  findOne(@Payload() id: number) {
+  findOne(@Payload('id') id: number) {
     return this.ordersService.findOne(id);
   }
 
