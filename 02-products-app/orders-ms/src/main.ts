8705d176966223f30c92e5fba5170f1af6cabc23
Jorge Matos <jisapjisap@gmail.com>
2024-06-19 16:39:11 +0200

    Orders Microservices Init

---
+++ 02-products-app/orders-ms/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { envs } from './config/envs';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+
+  const logger = new Logger('Orders-main')
+
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(envs.port);
+
+  logger.log(`Orders Microservices running on port ${envs.port}`)
+}
+bootstrap();
