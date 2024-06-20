af8c34de972b2f16817b7ef1a6bf08d951c31a13
Jorge Matos <jisapjisap@gmail.com>
2024-06-20 17:12:46 +0200

    Crear una nueva orden

--- 02-products-app/client-gateway/src/orders/orders.controller.ts
+++ 02-products-app/client-gateway/src/orders/orders.controller.ts
@@ -1,8 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Inject } from '@nestjs/common';
-import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
 import { ORDER_SERVICE } from 'src/config';
 import { ClientProxy } from '@nestjs/microservices';
+import { CreateOrderDto } from './dto';
 
 @Controller('orders')
 export class OrdersController {
--- 02-products-app/orders-ms/src/orders/dto/create-order.dto.ts
+++ 02-products-app/orders-ms/src/orders/dto/create-order.dto.ts
@@ -1,1 +1,26 @@
-export class CreateOrderDto {}
+import { OrderStatus } from "@prisma/client";
+import { IsBoolean, IsEnum, IsNumber, IsOptional, IsPositive } from "class-validator";
+import { OrderStatusList } from "../enum/order.enum";
+
+
+export class CreateOrderDto {
+
+  @IsNumber()
+  @IsPositive()
+  totalAmount: number;
+
+  @IsNumber()
+  @IsPositive()
+  totalItems: number;
+
+  @IsEnum(OrderStatusList, {
+    message: `Possible status values are ${OrderStatusList}` // Mensaje si hay error en el orderStatus proporcionado
+  })
+  @IsOptional()
+  status: OrderStatus = OrderStatus.PENDING;
+
+  @IsBoolean()
+  @IsOptional()
+  paid: boolean = false;
+  
+}
