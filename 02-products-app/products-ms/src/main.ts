a96924a6aaf97863cf6150d11277f821743bef4d
Jorge Matos <jisapjisap@gmail.com>
2024-06-24 15:44:16 +0200

    Products-ms Cambiar de TCP a NATS

--- 02-products-app/products-ms/src/main.ts
+++ 02-products-app/products-ms/src/main.ts
@@ -8,12 +8,24 @@ async function bootstrap() {
 
   const logger = new Logger('Main');
 
+  console.log(envs.natsServers);
+
+  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  //   AppModule,
+  //   {
+  //     transport: Transport.TCP,
+  //     options: {
+  //       port: envs.port
+  //     }
+  //   }
+  // );
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.TCP,
+      transport: Transport.NATS,
       options: {
-        port: envs.port
+        servers: envs.natsServers,
       }
     }
   );
